0e4163472681a28742ec323aae645422ec0f1c54
tksgml97 <tksgml97@gmail.com>
2024-07-10 03:44:41 +0900

    no message

---
+++ PathFinding/Assets/Script/NavMeshBuilder.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DPathFinder
+{
+	class NavMeshBuilder
+	{
+		static NavMesh Build()
+		{
+			return null;
+		}
+	}
+}
