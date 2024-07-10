52946fe45ade4c19b43af5249cc3609c96152802
tksgml97 <tksgml97@gmail.com>
2024-07-11 03:35:15 +0900

    navmesh 데이터

--- PathFinding/Assets/Script/PathData.cs
+++ PathFinding/Assets/Script/PathData.cs
@@ -4,7 +4,20 @@ using UnityEngine;
 
 namespace DPathFinder
 {
+    public struct NavLinkInfo
+	{
+        public int link_idx;
+        public int size;
+	}
+    public class NavPolygon
+    {
+        public int[] vertex_idx;
+        public List<NavLinkInfo> link_polygons;
+    }
+
     public class NavMesh
     {
+        public Vector3[] vertices;
+        public List<NavPolygon> nav_polys;
     }
 }
